86c0d6fb154332a25b191745ff4d74f0d3be59c1
Bohdan <bogdanbelyj@gmail.com>
2026-03-15 19:12:52 -0600

    Solution

--- settings.py
+++ settings.py
@@ -3,8 +3,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# SECURITY WARNING: Modify this secret key if using in production!
-SECRET_KEY = "6few3nci_q_o@l1dlbk81%wcxe!*6r29yu629&d97!hiqat9fa"
 
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
@@ -18,3 +16,4 @@ DATABASES = {
 USE_TZ = False
 
 INSTALLED_APPS = ("db",)
+
